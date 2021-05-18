6ca2f62874c6a8b8bbed8cff2ac062c562c3d922
Jared Dellitt <jareddellitt@users.noreply.github.com>
2021-05-18 08:26:49 -0500

    Add example of creating multiple parts in parallel using the async API methods (#6)

--- src/main/java/com/vertexvis/example/PartCreator.java
+++ src/main/java/com/vertexvis/example/PartCreator.java
@@ -1,44 +1,57 @@
 package com.vertexvis.example;
 
+import com.vertexvis.ApiCallback;
 import com.vertexvis.ApiClient;
 import com.vertexvis.api.PartsApi;
 import com.vertexvis.api.TranslationInspectionsApi;
 import com.vertexvis.model.*;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+import static com.vertexvis.example.CallbackUtil.execute;
 
 class PartCreator {
-  private final TranslationInspectionsApi tiApi;
-  private final PartsApi parts;
+    private final TranslationInspectionsApi tiApi;
+    private final PartsApi parts;
 
-  public PartCreator(ApiClient client) {
-    this.parts = new PartsApi(client);
-    this.tiApi = new TranslationInspectionsApi(client);
-  }
+    public PartCreator(ApiClient client) {
+        this.parts = new PartsApi(client);
+        this.tiApi = new TranslationInspectionsApi(client);
+    }
 
-  public Part createPartFromFile(UUID id) throws InterruptedException {
-    QueuedJob qp = parts.createPart(getCreatePartRequest(id));
-    UUID partId =
-        JobPoller.pollUntilJobDone("part", () -> tiApi.getQueuedTranslation(qp.getData().getId()));
-    return parts.getPart(partId, null);
-  }
+    public Part createPartFromFile(UUID id) throws InterruptedException {
+        QueuedJob qp = parts.createPart(getCreatePartRequest(id));
+        UUID partId =
+                JobPoller.pollUntilJobDone("part", () -> tiApi.getQueuedTranslation(qp.getData().getId()));
+        return parts.getPart(partId, null);
+    }
 
-  private static CreatePartRequest getCreatePartRequest(UUID fileId) {
-    return new CreatePartRequest()
-        .data(
-            new CreatePartRequestData()
-                .type("part")
-                .attributes(
-                    new CreatePartRequestDataAttributes()
-                        .suppliedId("my-part-" + UUID.randomUUID())
-                        .suppliedRevisionId("my-part-rev-A"))
-                .relationships(
-                    new CreateGeometrySetRequestDataRelationships()
-                        .source(
-                            new FileRelationship()
-                                .data(
-                                    new FileRelationshipData()
-                                        .id(fileId)
-                                        .type(FileRelationshipData.TypeEnum.FILE)))));
-  }
+    public CompletableFuture<Part> createPartFromFileAsync(UUID id) {
+        CompletableFuture<QueuedJob> p = execute(cb -> parts.createPartAsync(getCreatePartRequest(id), cb));
+        CompletableFuture<UUID> partId = p.thenCompose(qj ->
+                JobPoller.pollUntilJobDoneAsync("part", () ->
+                        execute(cb -> tiApi.getQueuedTranslationAsync(qj.getData().getId(), cb))));
+
+        return partId.thenCompose(pId -> execute((ApiCallback<Part> cb) -> parts.getPartAsync(pId, null, cb)));
+    }
+
+    private static CreatePartRequest getCreatePartRequest(UUID fileId) {
+        return new CreatePartRequest()
+                .data(
+                        new CreatePartRequestData()
+                                .type("part")
+                                .attributes(
+                                        new CreatePartRequestDataAttributes()
+                                                .suppliedId("my-part-" + UUID.randomUUID())
+                                                .suppliedRevisionId("my-part-rev-A"))
+                                .relationships(
+                                        new CreateGeometrySetRequestDataRelationships()
+                                                .source(
+                                                        new FileRelationship()
+                                                                .data(
+                                                                        new FileRelationshipData()
+                                                                                .id(fileId)
+                                                                                .type(FileRelationshipData.TypeEnum.FILE)))));
+    }
 }
