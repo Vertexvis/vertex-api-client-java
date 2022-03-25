7c6618a86428caf572efa3572fb8e2efc82d2d9b
Madison Ehlers <madison.ehlers@vertexvis.com>
2022-03-25 11:22:04 -0500

    fix

--- src/main/java/com/vertexvis/example/SceneCreator.java
+++ src/main/java/com/vertexvis/example/SceneCreator.java
@@ -6,23 +6,7 @@ import static com.vertexvis.model.CameraFit.TypeEnum.FIT_VISIBLE_SCENE_ITEMS;
 import com.vertexvis.ApiClient;
 import com.vertexvis.api.SceneItemsApi;
 import com.vertexvis.api.ScenesApi;
-import com.vertexvis.model.AnyOfGeometrySetRelationshipPartRevisionRelationshipSceneRelationship;
-import com.vertexvis.model.CameraFit;
-import com.vertexvis.model.CreateSceneItemRequest;
-import com.vertexvis.model.CreateSceneItemRequestData;
-import com.vertexvis.model.CreateSceneItemRequestDataAttributes;
-import com.vertexvis.model.CreateSceneItemRequestDataRelationships;
-import com.vertexvis.model.CreateSceneRequest;
-import com.vertexvis.model.CreateSceneRequestData;
-import com.vertexvis.model.CreateSceneRequestDataAttributes;
-import com.vertexvis.model.Part;
-import com.vertexvis.model.PartDataRelationshipsPartRevisions;
-import com.vertexvis.model.PartRevisionRelationship;
-import com.vertexvis.model.QueuedJob;
-import com.vertexvis.model.Scene;
-import com.vertexvis.model.UpdateSceneRequest;
-import com.vertexvis.model.UpdateSceneRequestData;
-import com.vertexvis.model.UpdateSceneRequestDataAttributes;
+import com.vertexvis.model.*;
 
 import java.util.UUID;
 
@@ -77,9 +61,9 @@ class SceneCreator {
                     .type("scene")
                     .attributes(
                         new UpdateSceneRequestDataAttributes()
-                            .state(UpdateSceneRequestDataAttributes.StateEnum.COMMIT))));
+                            .state(UpdateSceneRequestDataAttributes.StateEnum.COMMIT)
                             .camera(
-                                new AnyOfCameraCameraFit(
+                                new AnyOfPerspectiveCameraOrthographicCameraCameraFit(
                                     new CameraFit().type(FIT_VISIBLE_SCENE_ITEMS))))));
 
     return scene;
