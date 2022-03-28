75aaf3a4a90432595ac19cc69f8453899ebdad29
Madison Ehlers <madison.ehlers@vertexvis.com>
2022-03-28 08:52:47 -0500

    bump api client

--- src/main/java/com/vertexvis/ApiClient.java
+++ src/main/java/com/vertexvis/ApiClient.java
@@ -187,7 +187,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("vertex-api-client-java/0.3.0");
+        setUserAgent("vertex-api-client-java/0.3.1");
 
         authentications = new HashMap<String, Authentication>();
     }
