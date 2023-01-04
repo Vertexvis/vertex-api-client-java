d5fbbc5ee436fa1d1249227c492b0da92731fe2c
Gerrick Bivins <gerrick.bivins@vertexvis.com>
2023-01-04 15:18:01 -0600

    no progress bar

--- src/main/java/com/vertexvis/example/CreateAssemblyFromRevisionsExample.java
+++ src/main/java/com/vertexvis/example/CreateAssemblyFromRevisionsExample.java
@@ -11,7 +11,6 @@ import com.vertexvis.model.CreateFileRequestDataAttributes;
 import com.vertexvis.model.FileMetadata;
 import com.vertexvis.model.Part;
 import com.vertexvis.model.PartDataRelationshipsPartRevisions;
-import me.tongfei.progressbar.ProgressBar;
 import picocli.CommandLine;
 
 import java.io.File;
@@ -51,38 +50,37 @@ public class CreateAssemblyFromRevisionsExample extends CommandLineOptions {
         SceneCreator sc = new SceneCreator(client);
 
 
-        List<CompletableFuture<Part>> futureParts = ProgressBar.wrap(getPartFiles().stream(),"Parts")//Stream.of("16000_16000.jt", "redo/sooo-many/2043904/2043904_2043904_200_10_10_2Instanced.tgz", "redo/cubey/160_160_1000_10_10_10Instanced.tgz")
+        List<CompletableFuture<Part>> futureParts = getPartFiles().stream()
                 .map(CreateAssemblyFromRevisionsExample::buildCreateFileReq)
                 .map(req -> createAndUploadFile(files, req)
                         .thenCompose(partId -> pc.createPartFromFileAsync(partId, req)))
                 .collect(Collectors.toList());
 
         CompletableFuture.allOf(futureParts.toArray(new CompletableFuture[0])).join();
-        List<Part> newParts = ProgressBar.wrap(futureParts.stream(),"Creating parts")
+        List<Part> newParts = futureParts.stream()
                 .map(CompletableFuture::join)
                 .collect(Collectors.toList());
         newParts.forEach(p -> logger.info("Created part: " + p.getData().getId() + " with name " +
                 p.getData().getAttributes().getName()));
 
         try {
-            Part assembly =
-                    ProgressBar.wrap(newParts.stream(),"Creating assembly ["+getAssemblyName()+"]")
-                            .map(part -> part.getData().getRelationships().getPartRevisions())
-                            .flatMap(Collection::stream)
-                            .map(PartDataRelationshipsPartRevisions::getId)
-                            .peek(uuid -> logger.info(uuid.toString()))
-                            .collect(Collectors.collectingAndThen(Collectors.toList(), (l) -> pc.createAssemblyFromRevisions(l, "gbiv-assembly" + UUID.randomUUID())))
-                            .handle((part, ex) -> {
-                                if (ex != null) {
-                                    if (ex.getCause() instanceof ApiException) {
-                                        logger.log(Level.SEVERE, ((ApiException) ex.getCause()).getResponseBody(), ex.getCause());
-                                    }
-                                    logger.log(Level.SEVERE, ex.getCause().getMessage(), ex);
-                                    return null;
-                                } else {
-                                    return part;
-                                }
-                            }).join();
+            Part assembly = newParts.stream()
+                    .map(part -> part.getData().getRelationships().getPartRevisions())
+                    .flatMap(Collection::stream)
+                    .map(PartDataRelationshipsPartRevisions::getId)
+                    .peek(uuid -> logger.info(uuid.toString()))
+                    .collect(Collectors.collectingAndThen(Collectors.toList(), (l) -> pc.createAssemblyFromRevisions(l, getAssemblyName())))
+                    .handle((part, ex) -> {
+                        if (ex != null) {
+                            if (ex.getCause() instanceof ApiException) {
+                                logger.log(Level.SEVERE, ((ApiException) ex.getCause()).getResponseBody(), ex.getCause());
+                            }
+                            logger.log(Level.SEVERE, ex.getCause().getMessage(), ex);
+                            return null;
+                        } else {
+                            return part;
+                        }
+                    }).join();
 
             logger.info("Created assembly: " + assembly.getData().getId() + " with name " +
                     assembly.getData().getAttributes().getName());
