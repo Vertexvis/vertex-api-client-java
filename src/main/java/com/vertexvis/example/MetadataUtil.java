fb25159a2b1adf57f3a40e0f64078c52f376471e
gbiv <gerrick.bivins@vertexvis.com>
2023-05-30 14:01:07 -0500

    sonar

--- src/main/java/com/vertexvis/example/MetadataUtil.java
+++ src/main/java/com/vertexvis/example/MetadataUtil.java
@@ -6,7 +6,7 @@ import java.time.OffsetDateTime;
 import java.util.AbstractMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.random.RandomGenerator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -24,16 +24,16 @@ public class MetadataUtil {
         return IntStream.range(0, numberOfMetadataProperties).mapToObj(i -> {
             // generate key that index of the number of metadata properties
             var key = "key_0" + (i + 1) + "/" + numberOfMetadataProperties;
-
-            var metadataType = ThreadLocalRandom.current().nextInt(MetadataType.values().length);
+            var random = RandomGenerator.getDefault();
+            var metadataType = random.nextInt(MetadataType.values().length);
             var value = switch (MetadataType.values()[metadataType]) {
                 case LONG ->
                         new AnyOfMetadataLongTypeMetadataFloatTypeMetadataDateTypeMetadataStringTypeMetadataNullType(
-                                new MetadataLongType().type("long").value(ThreadLocalRandom.current().nextLong()));
+                                new MetadataLongType().type("long").value(random.nextLong()));
 
                 case FLOAT ->
                         new AnyOfMetadataLongTypeMetadataFloatTypeMetadataDateTypeMetadataStringTypeMetadataNullType(
-                                new MetadataFloatType().type("float").value(ThreadLocalRandom.current().nextFloat()));
+                                new MetadataFloatType().type("float").value(random.nextFloat()));
                 case DATE ->
                         new AnyOfMetadataLongTypeMetadataFloatTypeMetadataDateTypeMetadataStringTypeMetadataNullType(
                                 new MetadataDateType().type("date").value(OffsetDateTime.now()));
@@ -44,7 +44,6 @@ public class MetadataUtil {
                         new AnyOfMetadataLongTypeMetadataFloatTypeMetadataDateTypeMetadataStringTypeMetadataNullType(
                                 new MetadataNullType().type("null"));
 
-                default -> throw new IllegalArgumentException("Invalid metadata type");
             };
             // return a map entry of key and value
             return new AbstractMap.SimpleEntry<>(key, value);
