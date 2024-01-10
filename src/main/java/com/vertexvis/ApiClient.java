640434d217cafafc3448e41ee7a6b5e3a9691c99
Jeff Murray <jeff.murray@vertexvis.com>
2024-01-10 16:29:27 -0600

    Patch instead of minor

--- src/main/java/com/vertexvis/ApiClient.java
+++ src/main/java/com/vertexvis/ApiClient.java
@@ -207,7 +207,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("vertex-api-client-java/0.8.0");
+        setUserAgent("vertex-api-client-java/0.7.4");
 
         authentications = new HashMap<String, Authentication>();
     }
