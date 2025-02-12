e67fa1eb4b02f7e31bb5864866f1a03ebffb3b88
Anders Sjoblom <anders.sjoblom@vertexvis.com>
2025-02-12 18:59:56 -0500

    Added placeholder unit test for new AnyOfs

---
+++ src/test/java/com/vertexvis/model/AnyOfPropertySetRelationshipPartRevisionRelationshipSceneItemRelationshipPartInstanceRelationshipTest.java
@@ -0,0 +1,45 @@
+package com.vertexvis.model;
+
+import com.google.gson.reflect.TypeToken;
+import com.vertexvis.JSON;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class AnyOfPropertySetRelationshipPartRevisionRelationshipSceneItemRelationshipPartInstanceRelationshipTest {
+
+    @Test
+    void deserializePropertySetRelationship() {
+    }
+
+    @Test
+    void serializePropertySetRelationship() {
+    }
+
+    @Test
+    void deserializePartRevisionRelationship() {
+    }
+
+    @Test
+    void serializePartRevisionRelationship() {
+    }
+
+    @Test
+    void deserializeSceneItemRelationship() {
+    }
+
+    @Test
+    void serializeSceneItemRelationship() {
+    }
+
+    @Test
+    void deserializePartInstanceRelationship() {
+    }
+
+    @Test
+    void serializePartInstanceRelationship() {
+    }
+}
---
+++ src/test/java/com/vertexvis/model/AnyOfPropertyStringTypePropertyDoubleTypePropertyLongTypePropertyDateTypeobjectTest.java
@@ -0,0 +1,45 @@
+package com.vertexvis.model;
+
+import com.google.gson.reflect.TypeToken;
+import com.vertexvis.JSON;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class AnyOfPropertyStringTypePropertyDoubleTypePropertyLongTypePropertyDateTypeobjectTest {
+
+    @Test
+    void deserializePropertyStringType() {
+    }
+
+    @Test
+    void serializePropertyStringType() {
+    }
+
+    @Test
+    void deserializePropertyDoubleType() {
+    }
+
+    @Test
+    void serializePropertyDoubleType() {
+    }
+
+    @Test
+    void deserializePropertyLongType() {
+    }
+
+    @Test
+    void serializePropertyLongType() {
+    }
+
+    @Test
+    void deserializePropertyDateType() {
+    }
+
+    @Test
+    void serializePropertyDateType() {
+    }
+}
